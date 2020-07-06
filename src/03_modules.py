be44e6b013744b8bb3cb5b38a736ec7f4bc2c28d
Elysia Gabe <elysiagabe@gmail.com>
2020-07-06 16:06:25 -0700

    Intro to Python 1 (Day 1)

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,12 +11,14 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("os platform: ", sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print("python version: ", sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
